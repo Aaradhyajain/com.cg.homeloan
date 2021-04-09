a2f2744f37ba786d8504b619f106344f38140539
Aaradhya_Pc <Aaradhya_Pc@AaradhyaPc>
2021-04-09 12:22:55 +0530

    commit

---
+++ com.cg.homeloan/src/main/java/com/cg/homeloan/entities/EMI.java
@@ -0,0 +1,15 @@
+package com.cg.homeloan.entities;
+
+import java.time.LocalDate;
+
+public class EMI {
+	
+	private long EMIId;
+	private LocalDate dueDate;
+	private double emiAmount;
+	private double loanAmount;
+	private double interestAmount;
+	private long loanAgreementId;
+	
+
+}
