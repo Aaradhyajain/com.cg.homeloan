622c8860f6d09f1b36673b59ed1ca5f44b7ab9c7
Aaradhya Jain <51756656+Aaradhyajain@users.noreply.github.com>
2021-04-12 16:09:35 +0530

    Delete java/com/cg/homeloan directory

--- java/com/cg/homeloan/services/FinanceVerificationService.java
+++
@@ -1,9 +0,0 @@
-package com.cg.homeloan.services;
-
-import com.cg.homeloan.entities.LoanApplication;
-
-public class FinanceVerificationService  {
-public void updateStatus(LoanApplication loanApplication) {
-}
-
-}
