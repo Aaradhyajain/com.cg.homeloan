19c25ab9998b03abb57ee10bb7ffa35bb0f60aa5
Rohit5332 <46932313+Rohit5332@users.noreply.github.com>
2021-04-15 16:39:06 +0530

    Delete com.cg.homeloan directory

--- com.cg.homeloan/src/main/java/com/cg/homeloan/services/IFinanceVerificationService.java
+++
@@ -1,9 +0,0 @@
-package com.cg.homeloan.services;
-
-import com.cg.homeloan.entities.LoanApplication;
-import com.cg.homeloan.exceptions.FinanceVerificationException;
-
-public interface IFinanceVerificationService  {
-public LoanApplication updateStatus(LoanApplication loanApplication) throws FinanceVerificationException ;
-
-}
