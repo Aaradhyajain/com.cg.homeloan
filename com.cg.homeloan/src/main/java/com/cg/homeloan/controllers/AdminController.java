feaea6c3214f0fb9b22a801b0576ca8ab1f1dc8f
Aaradhy Jain <ajainmonty@gmail.com>
2021-04-17 15:18:06 +0530

    Right Controllers

--- com.cg.homeloan/src/main/java/com/cg/homeloan/controllers/AdminController.java
+++ com.cg.homeloan/src/main/java/com/cg/homeloan/controllers/AdminController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,22 +15,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cg.homeloan.entities.Admin;
+import com.cg.homeloan.entities.Customer;
 import com.cg.homeloan.entities.FinanceVerificationOfficer;
 import com.cg.homeloan.entities.LandVerificationOfficer;
 import com.cg.homeloan.entities.LoanApplication;
 import com.cg.homeloan.exceptions.AdminApprovalException;
+import com.cg.homeloan.exceptions.CustomerNotFoundException;
 import com.cg.homeloan.exceptions.LoanApplicationNotFoundExcption;
+import com.cg.homeloan.services.CustomerService;
 import com.cg.homeloan.services.IAdminService;
 import com.cg.homeloan.services.IFinanceVerificationService;
 import com.cg.homeloan.services.ILandVerificationService;
 import com.cg.homeloan.services.ILoanApplicationService;
 
 @RestController
-@RequestMapping("/homeloan")
+@RequestMapping("/homeloan/admin")
 public class AdminController {
 
 	@Autowired
 	IAdminService adminService;
+	
+	@Autowired
+	CustomerService customerService;
 	
 	@Autowired
 	IFinanceVerificationService financeVerificationService;
@@ -39,17 +46,7 @@ public class AdminController {
 	
 	@Autowired
 	ILoanApplicationService loanApplicationService;
-
-	@GetMapping("/Admin")
-	public ResponseEntity<List<Admin>> viewAllAdmin() {
-		return new ResponseEntity<>(adminService.getAllAdmin(), HttpStatus.OK);
-	}
-
-	@GetMapping("/Admin/{userId}")
-	public ResponseEntity<Admin> viewAdmin(@PathVariable("userId") int userId) {
-		return new ResponseEntity<>(adminService.getAdmin(userId), HttpStatus.FOUND);
-	}
-
+	
 	@PostMapping("/addAdmin")
 	public ResponseEntity<Admin> addAdmin(@RequestBody Admin admin) {
 		return new ResponseEntity<>(adminService.addAdmin(admin), HttpStatus.OK);
@@ -65,6 +62,26 @@ public class AdminController {
 		return new ResponseEntity<>(landVerificationService.addLandOfficer(officer),HttpStatus.OK);
 	}	
 	
+	@GetMapping("/{userId}")
+	public ResponseEntity<Admin> viewAdmin(@PathVariable("userId") int userId) {
+		return new ResponseEntity<>(adminService.getAdmin(userId), HttpStatus.OK);
+	}
+	
+	@GetMapping("/Admins")
+	public ResponseEntity<List<Admin>> viewAllAdmin() {
+		return new ResponseEntity<>(adminService.getAllAdmin(), HttpStatus.OK);
+	}
+
+	@GetMapping("/customers")
+	public ResponseEntity<List<Customer>> viewAllCustomers(){
+		return new ResponseEntity<>(customerService.getAllCustomers(),HttpStatus.OK);
+	}
+	
+	@GetMapping("/LoanApplications")
+	public ResponseEntity<List<LoanApplication>> getAllLoanApplications() {
+		return new ResponseEntity<>(loanApplicationService.getAllLoanApplication(), HttpStatus.OK);
+	}
+	
 	@PutMapping("/updateAdminVerificationStatus/{loanApplicationId}")
 	public ResponseEntity<LoanApplication> updateAdminStatus(@PathVariable int loanApplicationId) throws AdminApprovalException, LoanApplicationNotFoundExcption {
 		return new ResponseEntity<>(loanApplicationService.updateAdminStatus(loanApplicationId), HttpStatus.OK);
@@ -76,4 +93,10 @@ public class AdminController {
 	public ResponseEntity<Boolean> isValidAdmin(@PathVariable String username,@PathVariable String password) {
 		return new ResponseEntity<>(adminService.isValidAdmin(username, password),HttpStatus.OK);
 	}
+	
+	@DeleteMapping("/deleteCustomer/{userId}")
+	public ResponseEntity<Customer> deleteCustomer(@PathVariable int userId) throws CustomerNotFoundException{
+		return new ResponseEntity<>(customerService.deleteCustomer(userId),HttpStatus.OK);
+	}
+	
 }
--- com.cg.homeloan/src/main/java/com/cg/homeloan/controllers/LoanApplicationController.java
+++ com.cg.homeloan/src/main/java/com/cg/homeloan/controllers/LoanApplicationController.java
@@ -1,52 +1,39 @@
 package com.cg.homeloan.controllers;
 
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.cg.homeloan.entities.LoanApplication;
-import com.cg.homeloan.exceptions.CustomerNotFoundException;
 import com.cg.homeloan.exceptions.LoanApplicationNotFoundExcption;
 import com.cg.homeloan.services.ILoanApplicationService;
 
 @RestController
-@RequestMapping("/homeloan")
+@RequestMapping("/homeloan/loanApplication")
 public class LoanApplicationController {
 
 	@Autowired
-	ILoanApplicationService loanapplicationservice;
+	ILoanApplicationService loanApplicationService;
 
-	@GetMapping("/LoanApplications")
-	public ResponseEntity<List<LoanApplication>> getAllLoanApplications() {
-		return new ResponseEntity<>(loanapplicationservice.getAllLoanApplication(), HttpStatus.OK);
-	}
-
-	@GetMapping("/LoanApplication/{loanApplicationId}")
+	@GetMapping("/{loanApplicationId}")
 	public ResponseEntity<LoanApplication> getLoanApplication(@PathVariable("loanApplicationId") int loanApplicationId) throws LoanApplicationNotFoundExcption {
-		return new ResponseEntity<>(loanapplicationservice.getLoanApplication(loanApplicationId), HttpStatus.FOUND);
-	}
-
-	@PostMapping("/addLoanApplication/{userId}/{loanAppliedAmount}")
-	public ResponseEntity<LoanApplication> addLoanApplication(@PathVariable int userId, @PathVariable double loanAppliedAmount) throws CustomerNotFoundException {
-		return new ResponseEntity<>(loanapplicationservice.addLoanApplication(userId,loanAppliedAmount), HttpStatus.OK);
+		return new ResponseEntity<>(loanApplicationService.getLoanApplication(loanApplicationId), HttpStatus.OK);
 	}
 
 	@PutMapping("/updateLoanApplication")
 	public ResponseEntity<LoanApplication> updateLoanApplication(@PathVariable int loanApplicationId, @RequestBody LoanApplication loanApplication) throws LoanApplicationNotFoundExcption {
-		return new ResponseEntity<>(loanapplicationservice.updateLoanApplication(loanApplicationId,loanApplication), HttpStatus.OK);
+		return new ResponseEntity<>(loanApplicationService.updateLoanApplication(loanApplicationId,loanApplication), HttpStatus.OK);
 	}
 
 	@DeleteMapping("/deleteLoanApplication/{loanApplicationId}")
 	public ResponseEntity<LoanApplication> deleteLoanApplication(@PathVariable int loanApplicationId) throws LoanApplicationNotFoundExcption {
-		return new ResponseEntity<>(loanapplicationservice.deleteLoanApplication(loanApplicationId), HttpStatus.FOUND);
+		return new ResponseEntity<>(loanApplicationService.deleteLoanApplication(loanApplicationId), HttpStatus.OK);
 	}
 
 }
